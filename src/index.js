89abd6d5d91cb26f1dab442f5c8968898bde78bb
sergio <emanuelledesma.9427@gmail.com>
2021-12-29 20:17:35 -0300

    video 5

--- src/index.js
+++ src/index.js
@@ -1,33 +1,45 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
 // SDK de Mercado Pago
 const mercadopago = require ('mercadopago');
 
+//middelware
+app.use(bodyParser.urlencoded({ extended: false }))
+
 // Agrega credenciales
 mercadopago.configure({
-  access_token: 'APP_USR-4993256941478827-122822-1a2430fe21a8d1141f3944df9b7c326d-1047833761'
+  access_token: 'TEST-1044698736498330-122720-081f4e1650450a5ea872a75070953b9c-210694965'
 });
 
 //Routes
 app.get('/checkout',(req, res)=>{
-// Crea un objeto de preferencia orden de compra
-let preference = {
+  // Crea un objeto de preferencia orden de compra
+  let preference = {
     items: [
       {
         title: 'Mi producto',
         unit_price: 100,
-        quantity: 1,
+        quantity: 1
       }
     ]
   };
-  
   mercadopago.preferences.create(preference)
-  .then(function(response){
-    //aca va la accion responder al cliente
-  }).catch(function(error){
-    console.log(error);
-  });
+  .then(response=>{
+console.log(response)
+  })
+  .catch((error)=>{
+    console.log(error)
+  })
+  res.send('checkout')
+  
+//   .then(function(response){
+//     //aca va la accion responder al cliente
+//     console.log(response.body);
+//     res.send('checkout');
+//   }).catch(function(error){
+//     console.log(error);
+//   });
 })
 
 
